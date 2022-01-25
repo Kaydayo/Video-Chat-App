7201993926a93250a28c3b4049e68d4556e0b93f
kaydayo <olukayodedayo100@gmail.com>
2022-01-25 02:21:38 +0100

    🖼  implemented frontend with ejs

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const server = require("http").Server(app);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World")
+    res.render('room')
 })
+server.listen(5000)
